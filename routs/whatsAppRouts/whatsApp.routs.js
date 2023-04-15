2c80b2a9a6f93937f9b09865a53648bba5746c25
VikalpC2001 <vikalp0810@gmail.com>
2023-04-16 00:04:40 +0530

    orignel 8

--- routs/whatsAppRouts/whatsApp.routs.js
+++ routs/whatsAppRouts/whatsApp.routs.js
@@ -17,7 +17,7 @@
 'use strict';
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
-import axios from 'axios';
+const axios=require("axios");
 const fs = require('fs');
 const { type } = require('os');
 require('dotenv').config();
